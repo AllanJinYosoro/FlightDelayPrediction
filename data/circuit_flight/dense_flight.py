289e6a50c2de12d1ed5d5312dea9bb4eff7c4943
AllanJin <yosozura@163.com>
2024-04-21 16:04:39 +0800

    Add SARIMA for circuit_flight

--- data/circuit_flight/dense_flight.py
+++ data/circuit_flight/dense_flight.py
@@ -6,14 +6,14 @@ def check_dense_flight(series):
 
     return is_dense
 
-    
-data = pd.read_parquet('./data/data_preprocessed.parquet')
-data['FLIGHT_ID'] = data['AIRLINE'] + data['FLIGHT_NUMBER'].astype(str)
-data['Date'] = pd.to_datetime(data['Date'])
+def get_dense_flights_data(path = './data/data_preprocessed.parquet'):
+    data = pd.read_parquet(path)
+    data['FLIGHT_ID'] = data['AIRLINE'] + data['FLIGHT_NUMBER'].astype(str)
+    data['Date'] = pd.to_datetime(data['Date'])
 
-dense_flight = data.groupby('FLIGHT_ID')['Date'].apply(check_dense_flight).reset_index()
-dense_flight = dense_flight.loc[dense_flight['Date'] == True]
+    dense_flight = data.groupby('FLIGHT_ID')['Date'].apply(check_dense_flight).reset_index()
+    dense_flight = dense_flight.loc[dense_flight['Date'] == True]
 
-dense_flight_ids = dense_flight['FLIGHT_ID'].tolist()
-dense_data = data[data['FLIGHT_ID'].isin(dense_flight_ids)]
-dense_data.to_csv('dense_flights_data.csv')
+    dense_flight_ids = dense_flight['FLIGHT_ID'].tolist()
+    dense_data = data[data['FLIGHT_ID'].isin(dense_flight_ids)]
+    return dense_data
